db8dcd114b1c4ebaeed8c3c85917d860ab7d8583
souvik debbarma <souvikdbarma23@gmail.com>
2024-10-15 22:26:14 +0530

    custom colors has been set

--- Frontend/src/components/Header/Header.jsx
+++ Frontend/src/components/Header/Header.jsx
@@ -1,27 +1,63 @@
+import { Container, Row } from 'reactstrap';
+import { NavLink, Link } from 'react-router-dom';
 
-import { Container, Row, } from 'reactstrap'
+
+const Nav__links=[
+  {
+    path:'/home',
+    display:'Home'
+  },
+  {
+    path:'/about',
+    display:'About'
+  },
+  {
+    path:'/blog',
+    display:'Blog'
+  },
+  {
+    path: '/contact',
+    display:'Contact'
+  },
+]
 
 const Header = () => {
   return (
-    <header>
-      <Container className="bg-gray-900 text-white p-4">
-        {/* Top Row */}
+    <header className="bg-custom-darkvoid py-4">
+      <Container>
+        {/* Top row */}
         <Row>
-          <div>
-            {/* Left side: Phone number and email */}
-        <div>
-          <p>📞 +123 456 7890 | ✉️ email@example.com</p>
-        </div>
-          </div>
+          {/* Left side: Contact Info */}
+
+          {/* Right side: Social Media Icons */}
+          
         </Row>
 
         {/* Bottom Row */}
         <Row>
-          Hellow
+          <div>
+            {/* logo */}
+          <div className="logo">
+            <button className='font-sans inter-700'>Souvik.</button>
+          </div>
+          {/* logo end */}
+
+          {/* menu start */}
+          <div className="navigation ">
+            <ul className="">
+              {Nav__links.map((item, index) => (
+                <li className="nav__item" key={index}>
+                  <NavLink to={item.path}>{item.display}</NavLink>
+                </li>
+              ))}
+            </ul>
+          </div>
+          {/* menu end */}
+          </div>
         </Row>
       </Container>
     </header>
-  )
-}
+  );
+};
 
 export default Header;
--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
     content: ['./src/**/*.{html,js,jsx}'],
     theme: {
-      extend: {},
+      extend: {
+        fontFamily: {
+          'inter': ['Inter', 'sans-serif']
+        },
+        colors: {
+          'custom-darkvoid': '#151419',
+          'custom-liquidlava': '#F56E0F',
+          'custom-slateGrey': '#262626',
+          'custom-gluongrey': '#1B1B1E',
+          'custom-snow': '#FBFBFB',
+          'custom-DustyGrey': '#FBFBFB'
+        }
+      },
     },
     plugins: [],
   };
